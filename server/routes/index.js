1d6d0cda5d7f7312c1e7ed28b57cfcf743b1fb01
Dominic Muttel <dominic.muttel@gmail.com>
2015-08-24 16:20:37 -0600

    first commit -  needs some refactoring

---
+++ server/routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Shuffle and Chunk' });
+});
+
+router.post('/', function(req, res, next) {
+  var people = req.body.people.split(', ');
+  var number = req.body.number;
+  var shuffle = shuffleChunk(people, number);
+  res.render('index', {title: 'Shuffle and Chunk',results: shuffle});
+});
+
+function shuffle(array) {
+ var counter = array.length, temp, index;
+ while (counter > 0) {
+     index = Math.floor(Math.random() * counter);
+     counter--;
+     temp = array[counter];
+     array[counter] = array[index];
+     array[index] = temp;
+ }
+ return array;
+}
+
+function chunk(shuffledArray, size) {
+ var chunks = [],
+     i = 0,
+     n = shuffledArray.length,
+     num = n/size;
+ while (i < n) {
+   chunks.push(shuffledArray.slice(i, i += num));
+ }
+ for (i = 0; i < chunks.length; i++){
+  chunks[i] = chunks[i].join(", ");
+ }
+ return chunks;
+}
+
+function shuffleChunk(inputArray, num) {
+ var shuffledArray = shuffle(inputArray);
+ return chunk(shuffledArray, num);
+}
+
+module.exports = router;
